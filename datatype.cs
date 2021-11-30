45126b5961816a6a1e2db88caf36ce924aec4b17
lhenslee <lhenslee@vols.utk.edu>
2021-11-30 15:11:05 -0800

    first commit

---
+++ datatype.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace DataTypeApplication {
+  class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Size of int: {0}", sizeof(int));
+      Console.ReadLine();
+    }
+  }
+}
